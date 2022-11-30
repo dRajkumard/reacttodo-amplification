5d4514c37f9ae5a62271bda760e59a29c85c7bfb
unknown <rajkumar.d408@gmail.com>
2022-11-30 15:10:43 +0530

    push request

--- web/src/App.js
+++ web/src/App.js
@@ -1,35 +1,74 @@
- import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import * as tasksLib from "./lib/tasks";
+import { me } from "./lib/auth";
 
- import CreateTask from "./CreateTask";
- import Tasks from "./Tasks";
- function App() {
-  const [tasks, setTasks] = useState([]);
+import Auth from "./Auth";
+import CreateTask from "./CreateTask";
+import Tasks from "./Tasks";
+function App() {
+    const [user, setUser] = useState();
+    const [tasks, setTasks] = useState([]);
+  
+    useEffect(() => {
+      async function getUser() {
+        const result = await me();
+        setUser(result);
+      }
+      getUser();
+    }, [setUser]);
 
- const createTask = (text, id) => ({
-   id,
-   text,
-   completed: false,
- });
+    const setUserFetchTasks = async (user) => {
+        setUser(user);
+        if (!user) return;
+        const result = await tasksLib.getAll(user.id);
+        setTasks(result);
+      };
+      
+      useEffect(() => {
+        async function getUser() {
+          const result = await me();
+      
+          setUserFetchTasks(result);
+        }
+        getUser();
+  
+      }, [setUser, setTasks]);
 
- const addTask = (task) => {
-   const temp = [...tasks];
-   temp.push(createTask(task, tasks.length));
-   setTasks(temp);
- };
+   const addTask = async (task) => {
+    const newTask = await tasksLib.create(task, user.id);
+    if (!newTask) return;
+    const temp = [...tasks];
 
- const toggleCompleted = (id) => {
-   let temp = [...tasks];
-   const i = temp.findIndex((t) => t.id === id);
-   temp[i].completed = !temp[i].completed;
-   setTasks(temp);
- };
- return (
-  <div>
-    <CreateTask addTask={addTask} />
-    <Tasks tasks={tasks} toggleCompleted={toggleCompleted} />
-  </div>
-);
+    temp.push(newTask);
+    setTasks(temp);
+  };
+
+
+   const toggleCompleted = async (task) => {
+    const updatedTask = await tasksLib.update(task);
+    if (!updatedTask) return;
+    let temp = [...tasks];
+
+    const i = temp.findIndex((t) => t.id === updatedTask.id);
+ 
+    temp[i] = updatedTask;
+    setTasks(temp);
+  };
+  return (
+    <div>
+      {user ? (
+        <div>
+          <CreateTask addTask={addTask} />
+          <Tasks tasks={tasks} toggleCompleted={toggleCompleted} />
+        </div>
+      ) : (
+
+        <Auth setUser={setUserFetchTasks} />
+      )}
+    </div>
+  );
+
 }
 
 export default App;
